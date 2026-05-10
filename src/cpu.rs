0fa2d753198e15a123141353588b399dd0aded3c
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2026-05-10 10:58:31 -0600

    added execute method

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,1 +1,18 @@
+use crate::instruction::{ArithmeticTarget, Instruction};
+
 pub struct Cpu;
+impl Cpu {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::ADD(target) => match target {
+                ArithmeticTarget::A => todo!(),
+                ArithmeticTarget::B => todo!(),
+                ArithmeticTarget::C => todo!(),
+                ArithmeticTarget::D => todo!(),
+                ArithmeticTarget::E => todo!(),
+                ArithmeticTarget::H => todo!(),
+                ArithmeticTarget::L => todo!(),
+            },
+        }
+    }
+}
